73765e9e5bb97d4174131a95040e266af8897af4
NickBarraclough <46543465+NickBarraclough@users.noreply.github.com>
2020-10-06 13:44:47 -0700

    Update server.js

--- Benny's List/server.js
+++ Benny's List/server.js
@@ -13,6 +13,7 @@ var exphbs = require('express-handlebars');
 var port = process.env.PORT || 3000;
 var postData = require('./postData.json');
 var post;
+
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 
 app.set('view engine', 'handlebars');
@@ -22,25 +23,28 @@ app.use(express.static('public'));
 app.get('', function(req, res){
 	post = postData;
 	res.render('photoCards', {post});
-
 });
 
+// Grab post specified by post number 
 app.get('/:postNumber', function(req, res, next){
-
 	var postNumber = req.params.postNumber;
 	
 	if(postNumber > 0 && postNumber <= 8){
+		
 		post = [postData[postNumber - 1]];
 		res.status(200).render('photoCards', {post});
+		
 	} else {
 		next();
 	}
 });
 
+// Get 404 page on all unrecognizable pages
 app.get('*', function (req, res) {
 	res.status(404).render('404');
 });
 
+// Listen to port 3000 unless specified by environment variable
 app.listen(port, function () {
   console.log("== Server is listening on port", port);
 });
